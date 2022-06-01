71ef01e6bff4227a4acc64ff3f24375292c6a8ac
IshaanReni <ishaanreni@gmail.com>
2022-06-01 16:54:34 +0100

    Added a ring buffer that has not been tested/peer-reviewed yet. Fingers crossed!

--- Verilog_Quartus_Project/DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/RING_BUFFER.v
+++ Verilog_Quartus_Project/DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/RING_BUFFER.v
@@ -1,17 +1,54 @@
-module RING_BUFFER(
+module RING_BUFFER( //pls 4give moi if this doesn't work:(
 	// global clock & reset
 	clk,
 	reset_n,
-	
+	// data flow
 	data_in,
+	data_out,
+	// pointer values to be called upon
+	head_in,
+	tail_in
 );
+	/* We need two pointers: one head pointer and one tail pointer. We replace 
+	the value at the head pointer and we read the value at the tail pointer.*/
+
+	// Parameter Declaration(s)
+	parameter CAPACITY = 640; // to hold a whole row of pixels
+	parameter DATA_WIDTH = 24; // to hold {r,g,b}
 
 	// Input Port(s)
 	input [DATA_WIDTH-1:0] data_in;
+	input head_in; // push the inputted value of the head to be operated on
+	input tail_in; // likewise
 
-	// Parameter Declaration(s)
-	parameter CAPACITY = 640;
-	parameter DATA_WIDTH = 24;
+	// Output Port(s)
+	output [DATA_WIDTH-1:0] data_out;
+	output headOut;
+	output tailOut;
+	 
+	// Memory block instantiation
+	reg [DATA_WIDTH-1:0] buffer_array [CAPACITY-1:0];
 
+	// Counter logic for pointers
+	always @ (posedge clk) begin
+		if (reset_n) // If the reset signal is high then bring the pointers back to the beginning
+			headOut <= 1;
+			tailOut <= 0;
+		else
+			//Let's update the value at the head pointer and push out the value at the tail pointer
+			buffer_array[head] <= data_in;
+			data_out <= buffer_array[tail];
+			
+			//Now let's increment the head and the tail pointers.
+			if (head_in != (CAPACITY-1))
+				headOut <= head_in + 1;
+			else //else condition should take care of resetting the head pointer. 
+				headOut <= 0;
+			if (tail_in != (CAPACITY-1))
+				tailOut <= tail_in + 1;
+			else //else condition should take care of resetting the tail pointer. 
+				tailOut <= 0;	
+	end
+	
 
-
+endmodule
